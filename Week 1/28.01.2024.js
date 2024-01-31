b6e7daea884579144fc8ab3664f103f92ab8a200
RohitMore123456 <134958943+RohitMore123456@users.noreply.github.com>
2024-01-31 11:42:50 +0530

    Create 28.01.2024.js

---
+++ Week 1/28.01.2024.js
@@ -0,0 +1,46 @@
+// datatypes
+//Name
+//Informtion
+//Age
+//number
+
+//Boolean Yes/No
+//string - "" '' ``
+//"true"
+//'Awdiz'
+//`12345`
+//predefined set of code
+console.log("Ingram Micro")
+console.log(typeof("Ingram@2023"))  
+console.log('Ingram Micro')
+console.log(typeof'True')
+console.log(`Ingram Micro`)
+console.log(typeof(`12.23`))
+console.log("Ingram Micro", typeof('Ingram Micro'), 'IT Cloud',`Distribution`)
+console.log(2+2)
+
+console.log(9792, typeof(9792))
+console.log(9792, typeof('9792'))
+
+console.log(97+92)
+console.log('97'+'92')
+//Boolean
+//true, false
+console.log(true, typeof(true))
+console.log(false, typeof(false))
+console.log("true", typeof("true"))
+
+//Variables
+//let
+//Const
+var CompanyName = "Ingram"
+var Worldwidepresence = 18
+console.log(CompanyName, Worldwidepresence)
+var value1 = 15
+var value2 = 20
+console.log(value1+value2)
+console.log(value1-value2)
+console.log(value1*value2)
+console.log(value1/value2)
+
+
