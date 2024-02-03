cf7d3520cd02c81511b64238bb46275bd23cf569
RohitMore123456 <134958943+RohitMore123456@users.noreply.github.com>
2024-02-03 12:37:31 +0530

    update

---
+++ Week 1/01.02.2024.js
@@ -0,0 +1,14 @@
+{
+    let myname = "Awdiz";
+    console.log(myname)
+}
+{
+    const mysurname = "Awdiz"
+    console.log(mysurname)
+}
+console.log(1>2)
+console.log(2<4)
+console.log(7<=7)
+console.log(9>=10)
+console.log(2=="4")
+console.log(2 === "421")
--- Week 1/02.02.2024.js
+++ Week 1/02.02.2024.js
@@ -1,14 +1,28 @@
-{
-    let myname = "Awdiz";
-    console.log(myname)
+//conditional statement
+//if else
+//switch
+//syntax
+//if(condition 2==3){Statement 1} else{statement 2}
+var value1 = 50
+var value2 = 40
+var value3 = 30
+
+if (value1>value2) {
+    console.log("value 1 is greater than value 2")
+} else if (value1>value3) {
+    console.log("Value 1 is greater than value 3")
+}  else {
+    console.log("value 1 is leeser than value 2")
 }
-{
-    const mysurname = "Awdiz"
-    console.log(mysurname)
+//Age limit >18 L
+//Age limit >80 No Lic
+//Age limit ==18 L L
+//Age limit <18 No Lic
+var age = 90;
+if (age >80) {
+    console.log ("No license to be granted")
+} else if (age == 18) {
+    console.log ("Learning license to be granted")
+} else {
+    console.log("License to be granted")
 }
-console.log(1>2)
-console.log(2<4)
-console.log(7<=7)
-console.log(9>=10)
-console.log(2=="4")
-console.log(2 === "421")
