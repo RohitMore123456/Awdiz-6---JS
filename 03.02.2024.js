05f19b5ee2162195b63e3348d8e1d56a5f5533f8
RohitMore123456 <134958943+RohitMore123456@users.noreply.github.com>
2024-02-04 21:59:38 +0530

    Update 03.02.2024.js

--- 03.02.2024.js
+++ 03.02.2024.js
@@ -1,7 +1,38 @@
-var count = 0
-for(var i=10; i<30; i++){
-    if(i % 2 == 1){
-        count +=i
-    }
+//datatypes - String, number, boolean
+//variables - var, let, const
+//comparison operator - <,>,<=,>=,==,===
+//logical operator && ||
+//conditional statements - if else, switch
+//Loops for while
+
+switch (40) {
+    case 10:
+        console.log("10")
+        break;
+    
+    case 20:
+        console.log("20")
+        break;
+
+    default:
+        console.log("Neither 10 nor 20")
+        break;
 }
-console.log(Count)
+for (var count = 1; count <= 100; count++){
+    console.log("1")
+}
+for (var count=2; count <=10; count+= 5){
+    console.log("Hi")
+}
+
+for (var i=440; i<=480; i+=10){
+    console.log(i)
+}
+
+for(var i=10; i>=1; i--){
+    console.log(i)
+}
+
+
+
+
