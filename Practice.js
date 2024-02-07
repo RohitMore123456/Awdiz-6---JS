70e0c90e838e1de7dd498c8826b74e75f70b3186
RohitMore123456 <134958943+RohitMore123456@users.noreply.github.com>
2024-02-07 21:11:32 +0530

    update

---
+++ Practice.js
@@ -0,0 +1,31 @@
+// console.log("Rohit")
+// console.log(typeof"Rohit@1234")
+// console.log('Rohit')
+// console.log(typeof('true'))
+// console.log(`Rohit`)
+// console.log(typeof(`12.23`))
+
+// console.log("2"+'2')
+// console.log(2+2)
+
+// console.log('1234', typeof '1234')
+// console.log(1234, typeof 1234)
+// console.log(true, typeof true)
+
+// var mobile = 114
+// var mobile2 = 112
+
+// console.log('mobile'+'mobile2')
+
+// var value = 50assign
+// console.log(value)
+// value = 60 reassign
+// console.log(value)
+// var value = 70redeclare
+// console.log(value)
+
+let value = 50
+console.log(value)
+value = 60
+console.log(value)
+let value = 70
