ea9083b98b16915b18ea81d7ac8b1d7586e7785c
RohitMore123456 <134958943+RohitMore123456@users.noreply.github.com>
2024-02-10 12:25:36 +0530

    update

--- Practice.js
+++ Practice.js
@@ -1,13 +1,33 @@
-var array = [1,2,4,56,7,8,3,2,5,7];
-var target =  12;
-function findvalue(array, target) {
-    for (var i=0; i<=array.length-1; i++){
-        for (var j=1; j<=array.length-1; j++){
-            if (array[i]+array[j] == target){
-                console.log( array[i],array[j])
-            }
-        }
-    }  
+switch (20){
+    case (10): 
+    console.log("10")
+    break;
+    case (20):
+    console.log("20")
+    break;
+    default:
+        console.log("Neither 10 nor 20")
+        break;
 }
 
-findvalue (array, target)
+// for (var i=0; i<=100; i++){
+//     console.log(i)
+// }
+
+// for (var count=0; count<=10; count++){
+//     console.log("Hello")
+// }
+
+// for (var i=2; i<=10; i+=5){
+//     console.log(i)
+// }
+
+// for (var i=440; i<480; i+=10){
+//     console.log(i)
+// }
+
+//Q Print Nos form 10 to 1
+
+// for (var i=10; i>=1; i--){
+//     console.log(i)
+// }
