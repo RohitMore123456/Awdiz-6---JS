bf1b7f42c8bf3248ecc4898e934243f210e5db91
RohitMore123456 <134958943+RohitMore123456@users.noreply.github.com>
2024-02-13 20:23:48 +0530

    update

---
+++ week 4/10.02.2024.js
@@ -0,0 +1,31 @@
+// function checkPrime(num){
+//     for (let i = 2; i < num; i++){
+//       if(num % i === 0){
+//         }
+//       }
+//    }
+//    function detectPrime(arr){
+//        if (checkPrime){
+//            return true;
+//             }else{
+//               return false;
+//          }
+//  }
+//  console.log(detectPrime([15,110,7,22,30]));// true
+//  console.log(detectPrime([15,110,77,290,20]));// false
+
+//  var array  = [1,2,3,4]
+
+//  var myobject = {name: 'Awdiz', surname:"xyz", age:20, isopen: true, students: ["a", 'b']}
+
+// //  console.log(myobject, 'myobject')
+// console.log(myobject.students)
+// console.log(myobject.age)
+// console.log(myobject.surname)
+// console.log(myobject.name)
+
+// myobject["xyz"]= "xyz"
+// console.log(myobject)
+
+
+
