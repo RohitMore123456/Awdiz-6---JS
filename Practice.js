38991e64c5d64a58b2c3a125bab209e03ac0971e
RohitMore123456 <134958943+RohitMore123456@users.noreply.github.com>
2024-02-13 22:01:03 +0530

    update

--- Practice.js
+++ Practice.js
@@ -1,33 +1,4 @@
-switch (20){
-    case (10): 
-    console.log("10")
-    break;
-    case (20):
-    console.log("20")
-    break;
-    default:
-        console.log("Neither 10 nor 20")
-        break;
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
 }
-
-// for (var i=0; i<=100; i++){
-//     console.log(i)
-// }
-
-// for (var count=0; count<=10; count++){
-//     console.log("Hello")
-// }
-
-// for (var i=2; i<=10; i+=5){
-//     console.log(i)
-// }
-
-// for (var i=440; i<480; i+=10){
-//     console.log(i)
-// }
-
-//Q Print Nos form 10 to 1
-
-// for (var i=10; i>=1; i--){
-//     console.log(i)
-// }
