42946e60559c1137ebe85991c4fe7544684ec0ad
RohitMore123456 <134958943+RohitMore123456@users.noreply.github.com>
2024-02-15 01:47:47 +0530

    update

--- Practice.js
+++ Practice.js
@@ -22,9 +22,65 @@
 // }
 // console.log(count)
 
-var count = 0
-for (var i=0; i<=20; i++){
-    if (i%2 == 0)
-    count += i
+// var count = 0
+// for (var i=0; i<=20; i++){
+//     if (i%2 == 0)
+//     count += i
+// }
+// console.log(count)
+
+// var value1 = 10;
+// var value2 = 20;
+
+// function Rohit(num1,num2){
+//      console.log(num1+num2)
+// }
+
+// Rohit (value1, value2)
+
+// var myname  ;
+// if (myname == undefined){
+//     myname = "Rohmo"
+//     console.log(myname)
+// } else {
+//     console.log("Name is undefined")
+// }
+
+// var count = 0;
+// for (var i=0; i<=10; i++){
+//     if (i % 2 == 0)
+//     count += i
+// }
+// console.log(count)
+
+// var count = 0;
+// for (var i=34; i<=87; i++){
+//     if (i % 2 == 1) {
+//         count += i
+//     } 
+// }
+
+// console.log(count)
+
+// function Addition(value1 , value2) {
+//     var count = 0;
+//     for (var i=value1; i<= value2; i++){
+//         if (i % 2 == 1) {
+//             count += i
+//         }
+//     }
+//     console.log(count)
+// }
+
+// Addition (34,40)
+
+function Factorial(value) {
+var count = value;
+for (var i=value - 1; i>1; i--){
+    count *= i
 }
-console.log(count)
+   return (count)
+}
+
+const answer = Factorial (3)
+console.log(answer)
