5dc28c1a90506697e351910f6c81d2fe380dc196
RohitMore123456 <134958943+RohitMore123456@users.noreply.github.com>
2024-02-14 18:07:34 +0530

    update

--- Practice.js
+++ Practice.js
@@ -1,4 +1,30 @@
-var count = 5;
-count = count+10;
-count =+ 20
+// for (var i = 0; i<11; i+=2){
+//      console.log(i)
+// }
+
+// for (var i=10; i>=1; i--){
+//     console.log(i)
+// }
+
+// for (var i=0; i<=10; i++){
+//     if (i % 2 == 0) {
+//         console.log(i, "- Even")
+//     } else {
+//         console.log(i, "- Odd")
+//     }
+// }
+
+// var count = 0
+// for (var i = 10; i<= 30; i++){
+//     if (i%2 == 1) {
+//         count += i
+//     }
+// }
+// console.log(count)
+
+var count = 0
+for (var i=0; i<=20; i++){
+    if (i%2 == 0)
+    count += i
+}
 console.log(count)
