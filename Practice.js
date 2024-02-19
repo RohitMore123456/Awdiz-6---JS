bedaf4c4f39f9d57ae96b3c9819adf9906a11040
RohitMore123456 <134958943+RohitMore123456@users.noreply.github.com>
2024-02-19 12:15:25 +0530

    update

--- Practice.js
+++ Practice.js
@@ -1,86 +1,62 @@
-// for (var i = 0; i<11; i+=2){
-//      console.log(i)
-// }
-
-// for (var i=10; i>=1; i--){
-//     console.log(i)
-// }
-
-// for (var i=0; i<=10; i++){
-//     if (i % 2 == 0) {
-//         console.log(i, "- Even")
-//     } else {
-//         console.log(i, "- Odd")
-//     }
-// }
+// //Print Addition of Odd numbers from 10 to 30
 
 // var count = 0
-// for (var i = 10; i<= 30; i++){
-//     if (i%2 == 1) {
-//         count += i
-//     }
-// }
+// for (var i = 10; i<=30; i++ ){
+//     if (i % 2 == 1)
+//     count += i
+// } 
 // console.log(count)
+
+//Print addition of odd numbers from 10 to 50
 
 // var count = 0
-// for (var i=0; i<=20; i++){
-//     if (i%2 == 0)
-//     count += i
-// }
-// console.log(count)
-
-// var value1 = 10;
-// var value2 = 20;
-
-// function Rohit(num1,num2){
-//      console.log(num1+num2)
-// }
-
-// Rohit (value1, value2)
-
-// var myname  ;
-// if (myname == undefined){
-//     myname = "Rohmo"
-//     console.log(myname)
-// } else {
-//     console.log("Name is undefined")
-// }
-
-// var count = 0;
-// for (var i=0; i<=10; i++){
-//     if (i % 2 == 0)
-//     count += i
-// }
-// console.log(count)
-
-// var count = 0;
-// for (var i=34; i<=87; i++){
-//     if (i % 2 == 1) {
+// for (var i= 10; i<= 50; i++){
+//     if (i % 2 == 1) 
 //         count += i
-//     } 
 // }
-
 // console.log(count)
 
-// function Addition(value1 , value2) {
-//     var count = 0;
-//     for (var i=value1; i<= value2; i++){
-//         if (i % 2 == 1) {
-//             count += i
-//         }
-//     }
-//     console.log(count)
+//Q - Addition of even numbers from 50 to 100
+// var count = 0
+// for (var i=50; i<=100; i++){
+//     if(i % 2 == 0)
+//     count+= i
+// }
+// console.log(count)
+
+// var count1 = 1;
+// count2 = count1 + 1;
+// count2++
+// console.log(count2)
+
+// var count = 5;
+// count = count-1;
+// count--
+// count-=
+// console.log(count)
+
+// function Awdiz(){
+//     console.log("Hello")
+// }
+// Awdiz()
+
+// var myname = "Hello"
+// console.log(myname)
+
+// var value1 = 20;
+// var value2 = 30;
+
+// function Myvalue(){
+//     console.log(value1+value2)
 // }
 
-// Addition (34,40)
+// Myvalue()
 
-function Factorial(value) {
-var count = value;
-for (var i=value - 1; i>1; i--){
-    count *= i
-}
-   return (count)
+var value1 = 20;
+var value2 = 50
+function Rohmo(abc,xyz){
+    console.log(abc, xyz)
 }
 
-const answer = Factorial (3)
-console.log(answer)
+Rohmo(value2,value1)
+
