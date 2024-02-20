591e7cd0e2ccdd10ed34f23918e1ab9320a32775
RohitMore123456 <134958943+RohitMore123456@users.noreply.github.com>
2024-02-20 11:16:44 +0530

    update

--- Practice.js
+++ Practice.js
@@ -52,11 +52,120 @@
 
 // Myvalue()
 
-var value1 = 20;
-var value2 = 50
-function Rohmo(abc,xyz){
-    console.log(abc, xyz)
-}
+// var value1 = 20;
+// var value2 = 50
+// function Rohmo(abc,xyz){
+//     console.log(abc, xyz)
+// }
 
-Rohmo(value2,value1)
+// Rohmo(value2,value1)
+
+//Print number from 1-10, even number with -even and odd number with - odd
+
+// for (var i=1; i<=10; i++){
+//     if (i % 2 == 0) {
+//         console.log(i, " - Even")
+//     } else {
+//         console.log(i, '- Odd')
+//     }
+// }
+
+//Print Addition of Odd numbers from 1-30
+
+// var count = 0
+// for (var i=0; i<=30; i++){
+//     if(i % 2 == 1)
+//     count += i
+// }
+// console.log(count)
+
+
+// function awdiz (){
+//     console.log("Hello")
+// }
+// awdiz()
+
+// function Addition(){
+//     var value1 = 10;
+//     var value2 = 20;
+//     console.log(value1+value2)
+// }
+// Addition()
+
+// var value1 = 50;
+// var value2 = 50;
+
+// function addition (value1, value2){
+//     console.log(value1 + value2)
+// }
+
+// addition(value1, value2)
+
+// function Rohmo (abc, xyz){
+//     console.log(abc +xyz)
+// }
+// Rohmo (75, 40)
+
+//Q Find addition of odd number from given range
+
+// function addition(value1, value2){
+//     var count = 0;
+//     for (var i=value1; i<= value2; i++){
+//         if (i % 2 == 1) {
+//             count += i
+//         }
+//     }
+//     return count;
+// }
+// const answer = addition (34,38)
+// console.log(answer)
+
+//Find factorial of 10
+
+// function fact(value1){
+//     var factor = 1
+//      for (var i=value1; i>=1; i--){
+//         factor*= i
+//      }
+//      return factor;
+// }
+// const answer = fact (5)
+// console.log(answer)
+
+// var student =  ["virat", "Rohit", "Rahul"];
+// console.log(student, "student")
+// student.push("Iyer")
+// console.log(student)
+
+//q Find the addition of Odd numbers form Given range (34,38)
+
+// function Additionn (value1, value2){
+//     var Ans = 0
+//     for (var i=value1; i<=value2; i++){
+//         if (i % 2 == 0) {
+//             Ans += i
+//         }
+        
+//     }
+//     console.log(Ans)
+// }
+// Additionn (2,6)
+    
+// // Q - (2,6)
+// var count = 0
+// for (var i=2; i<=6; i++){
+//     if(i % 2 == 1)
+//     count += i 
+// }
+// console.log(count)
+
+// function Odd (value1, value2){
+//     var count = 0;
+//     for (var i=value1; i<= value2; i++){
+//         if( i % 2 == 1)
+//         count+= i
+//     }
+//     console.log(count)
+// }
+// Odd (7,14)
 
